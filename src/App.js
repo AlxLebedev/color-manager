dbd922de28ac0a81241b82b0aa4db4c5e227a112
Alexey Lebedev <vorresti@ya.ru>
2021-04-05 17:51:15 +0300

    AddColorForm --> почти сделал адаптивный дизайн

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Header from './components/Header/index';
+import AddColorForm from './components/AddColorForm';
 import './global-styles/App.scss';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <main>
+        <AddColorForm/>
+      </main>
     </div>
   );
 }
---
+++ src/components/AddColorForm/index.jsx
@@ -0,0 +1,14 @@
+import './index.scss';
+
+const AddColorForm = () => {
+    return (
+        <form className="App__add-color-form add-color-form">
+            <label className="add-color-form__name-input-label" for="color-name">Color name</label>
+            <input className="add-color-form__name-input" type="text" id="color-name" placeholder="For example - deep gray"/>
+            <input className="add-color-form__color-picker" type="color" defaultValue="#D1A1FF"/>
+            <button className="add-color-form__add-button" type="button">ADD</button>
+        </form>
+    );
+};
+
+export default AddColorForm;
