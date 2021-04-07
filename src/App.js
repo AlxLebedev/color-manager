4a54991ac44c118535b90f24d62fab2299c491c6
Alexey Lebedev <vorresti@ya.ru>
2021-04-07 15:30:41 +0300

    AddColorsForm --> добавил функциональность для передачи в ColoursList

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
+import { useState } from 'react';
 import Header from './components/Header/index';
+import ColoursList from './components/ColoursList';
 import AddColorForm from './components/AddColorForm';
 import './global-styles/App.scss';
 
 function App() {
+  const [state, setState] = useState({ colors: [] });
+  
   const addColorHandler = (colorName, colorCode) => {
     const color = {
       colorName,
       colorCode
     };
-    console.log(color);
+    const colors = [
+      ...state.colors,
+      color
+    ];
+    setState({ colors });
   }
-
 
   return (
     <div className="App">
       <Header />
       <main>
         <AddColorForm addColorHandler={addColorHandler}/>
+        <ColoursList colors={state.colors}/>
       </main>
     </div>
   );
